b2fa51fd3bde77a0c385d1d2e53209af953a15e0
swapna reniguntla <swapnar.gitacc@gmail.com>
2024-04-22 22:26:28 +0530

    spring security basic example

---
+++ src/main/java/com/crimsonlogic/springbootbasicsecurityexample/SpringbootbasicsecurityexampleApplication.java
@@ -0,0 +1,13 @@
+package com.crimsonlogic.springbootbasicsecurityexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbasicsecurityexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbasicsecurityexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crimsonlogic/springbootbasicsecurityexample/SpringbootbasicsecurityexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crimsonlogic.springbootbasicsecurityexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbasicsecurityexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
